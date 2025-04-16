11c4e5babdbb4d8a275ef37d9cf6b5481a54431d
Art007kz <Artaru11kz>
2025-04-16 12:33:13 +0300

    Поправил параметр All в init

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,5 @@
+__all__ = ["register_message_handlers", "set_my_commands"]
+
 from .handlers import router as message_router
 from .bot_commands import set_my_commands
 from .callbacks import router as callback_router
@@ -5,5 +7,3 @@ from .callbacks import router as callback_router
 def register_message_handlers(dp):
     dp.include_router(message_router)
     dp.include_router(callback_router)
-
-all = ["register_message_handlers", "set_my_commands"]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from config.config import BOT_TOKEN
+from config import TOKEN
 from handlers import register_message_handlers, set_my_commands
 
 # Настройка логирования
@@ -16,7 +16,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
     register_message_handlers(dp)
